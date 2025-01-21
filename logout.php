57b6551c073b465c5e3c818797fb7f438c0b0618
mariel12K <141769344+mariel12K@users.noreply.github.com>
2025-01-21 16:23:04 -0400

    logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,6 @@ session_unset(); // Eliminar todas las variables de sesión
 session_destroy(); // Destruir la sesión actual
 
 // Redirigir al formulario de login
-header("Location: index.html");
+header("Location: index.php");
 exit;
 ?>
